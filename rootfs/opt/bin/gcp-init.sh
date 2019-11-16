2fa82c0a2542bc23bf1f4f3c1ac211957afb9356
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-11-16 22:20:16 +0100

    upd

--- rootfs/opt/bin/gcp-init.sh
+++ rootfs/opt/bin/gcp-init.sh
@@ -3,6 +3,7 @@
 CFG_FILE="$HOME/env-gcp.sh"
 if [ -f "$CFG_FILE" ]; then
     >&2 echo "ERROR: missing $CFG_FILE"
+    exit 1
 fi
 . "$HOME/env-gcp.sh"
 
--- rootfs/opt/bin/gcp-kubeconfig.sh
+++ rootfs/opt/bin/gcp-kubeconfig.sh
@@ -6,6 +6,7 @@ set -x
 CFG_FILE="$HOME/env-gcp.sh"
 if [ -f "$CFG_FILE" ]; then
     >&2 echo "ERROR: missing $CFG_FILE"
+    exit 2
 fi
 . "$HOME/env-gcp.sh"
 
