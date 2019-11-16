86682aac07ae80a434f63eed3a327a82ed407ea5
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-11-16 22:28:15 +0100

    upd

--- rootfs/opt/bin/clone-school.sh
+++ rootfs/opt/bin/clone-school.sh
@@ -11,4 +11,4 @@ git checkout -q 36f0aa615b10617fea5f75ad0bf69d89d35b4164
 git checkout -b school
 
 git clone https://github.com/luksa/kubernetes-in-action.git
-git clone https://gitlab.com/k8s-school/k8s-advanced
+git clone https://github.com/k8s-school/k8s-advanced
