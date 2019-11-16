8bfea6a9d2314aef2d4ec57df1190b5e447b85e4
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-11-16 22:25:23 +0100

    upd

--- rootfs/opt/bin/clone-school.sh
+++ rootfs/opt/bin/clone-school.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+cd $HOME
+
 REPO=kubernetes-up-and-running
 git clone https://github.com/$REPO/examples.git
 cd examples
 git checkout -q 36f0aa615b10617fea5f75ad0bf69d89d35b4164
 git checkout -b school
 
-git clone https://github.com/luksa/kubernetes-in-action.git $HOME
-git clone https://gitlab.com/k8s-school/k8s-advanced $HOME
+git clone https://github.com/luksa/kubernetes-in-action.git
+git clone https://gitlab.com/k8s-school/k8s-advanced
