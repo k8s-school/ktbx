47c5fa3fab76024a08725b5ee6b3b1b0f9897a6f
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-11-14 21:04:25 +0100

    Update current feature

--- _desk/rootfs/opt/bin/clone-school.sh
+++ _desk/rootfs/opt/bin/clone-school.sh
@@ -4,7 +4,7 @@ set -e
 
 cd "$HOME"
 
-REPOS="https://github.com/k8s-school/examples https://github.com/luksa/kubernetes-in-action https://github.com/k8s-school/k8s-advanced https://github.com/k8s-school/k8s-school"
+REPOS="https://github.com/k8s-school/examples https://github.com/k8s-school/k8s-advanced https://github.com/k8s-school/k8s-school https://github.com/k8s-school/openshift-advanced"
 
 for r in $REPOS
 do
