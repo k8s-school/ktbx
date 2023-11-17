ab2605610e8c27b09aed371356c14040dd32f797
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-11-17 13:15:09 +0100

    Update current feature

--- itest/test-nginx.sh
+++ itest/test-nginx.sh
@@ -3,5 +3,4 @@
 set -e
 
 echo "Connect to nginx application"
-kubectl run --generator=run-pod/v1 -it --image=busybox shell wget http://my-nginx
-# TODO try port forwad and curl
+kubectl run -it --image=busybox shell wget http://my-nginx
