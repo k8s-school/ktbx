71647c478d1a8f75dc9fc3324cb495c5d67ceb61
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-04-21 20:51:24 +0200

    Improve helm install script

--- resources/install-helm.sh
+++ resources/install-helm.sh
@@ -7,8 +7,8 @@
 
 set -euxo pipefail
 
-HELM_VERSION="3.3.3"
-wget -O /tmp/helm.tgz https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
+HELM_VERSION="3.14.4"
+curl -o /tmp/helm.tgz https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz
 
 cd /tmp
 tar zxvf /tmp/helm.tgz
