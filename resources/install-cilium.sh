29bf4efce22cda5afe0cd95d00f3ea1e0a1656f9
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-04-21 20:46:37 +0200

    Install cilium with helm

--- resources/install-cilium.sh
+++ resources/install-cilium.sh
@@ -6,10 +6,13 @@
 
 set -euxo pipefail
 
-# Retrieve Cilium clie version with:
+# Retrieve latest Cilium cli version with:
 # curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
 CILIUM_CLI_VERSION="v0.16.4"
 CILIUM_VERSION="1.15.2"
+
+# Install cilium cli
+CILIUM_VERSION="1.15.4"
 CLI_ARCH=amd64
 if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
 curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
@@ -17,7 +20,12 @@ sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
 sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
 rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 
-cilium install --version "$CILIUM_VERSION"
+# Install cilium
+helm repo add cilium https://helm.cilium.io/
+helm install cilium cilium/cilium --version $CILIUM_VERSION \
+   --namespace kube-system \
+   --set image.pullPolicy=IfNotPresent \
+   --set ipam.mode=kubernetes
 
 echo "Wait for cilium daemonset to be ready"
 kubectl rollout status -n kube-system --timeout=600s daemonset/cilium
