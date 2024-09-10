6f2cf2dc00f5be437421fc7d9199d54efe927b6a
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-09-10 11:43:14 +0200

    Update current feature

--- cmd/delete.go
+++ cmd/delete.go
@@ -7,12 +7,17 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/k8s-school/ktbx/internal"
 	"github.com/spf13/cobra"
+	// Add this import
 )
 
-func deleteCluster() {
+var clusterNamePattern string
+
+func deleteCluster(clusterName string) {
 
 	optName := ""
 	if clusterName != "" {
@@ -30,16 +35,58 @@ func deleteCluster() {
 	}
 }
 
+func getClusterByPattern(pattern string) []string {
+
+	cmd_tpl := "%v get clusters"
+	cmd := fmt.Sprintf(cmd_tpl, internal.Kind)
+
+	out, _, err := ExecCmd(cmd, false)
+	if err != nil {
+		slog.Error("Error while getting clusters", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Debug("Get clusters", "output", out)
+	r, _ := regexp.Compile(pattern)
+
+	clusterMatches := make([]string, 0)
+
+	clusters := strings.Split(out, "\n")
+
+	for _, cluster := range clusters {
+		if r.MatchString(cluster) {
+			clusterMatches = append(clusterMatches, cluster)
+		}
+	}
+
+	return clusterMatches
+
+}
+
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete a kind cluster",
 	Run: func(cmd *cobra.Command, args []string) {
-		slog.Info("Delete %v cluster")
-		deleteCluster()
+
+		if clusterNamePattern != "" {
+			// Get the output of "kind get clusters"
+			// Loop over the output and delete the clusters that match the pattern
+			slog.Info("Delete kind cluster with pattern", "pattern", clusterNamePattern)
+			clusters := getClusterByPattern(clusterNamePattern)
+			slog.Debug("Cluster matches", "clusters", clusters)
+
+		} else {
+
+			slog.Info("Delete kind cluster")
+			deleteCluster("")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(deleteCmd)
+
+	pattern := "pattern"
+	deleteCmd.PersistentFlags().StringVarP(&clusterNamePattern, pattern, "p", "", "delete cluster by name regexp pattern")
 }
