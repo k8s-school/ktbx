4610317ef790175b7bd8907dfe0699cf6f3be914
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-10-28 20:56:21 +0100

    Fix nginx tests

--- e2e/run-nginx.sh
+++ e2e/run-nginx.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-set -e
+set -euxo pipefail
 
 echo "Launch nginx application"
 kubectl create deployment my-nginx --image=nginx
 kubectl expose deployment my-nginx --port=80
 kubectl wait --for=condition=available --timeout=600s deployment/my-nginx
+
+echo "Connect to nginx application"
+kubectl run -it --image=nginx client --restart=Never -- curl http://my-nginx:80
--- e2e/test-nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo "Connect to nginx application"
-kubectl run -it --image=busybox shell wget http://my-nginx
