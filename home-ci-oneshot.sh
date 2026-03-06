260c9699fadf36cfcefba4f3bc1457b545ae2bca
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2026-03-06 15:42:40 +0100

    Update current feature

--- home-ci-oneshot.sh
+++ home-ci-oneshot.sh
@@ -1,1 +1,1 @@
-rm -rf /tmp/home-ci &&  home-ci run -v 3 -c $PWD/../home-ci/examples/ktbx.yaml -b main
+rm -rf /tmp/home-ci &&  home-ci run -v 3 -c $PWD/../home-ci/examples/ktbx.yaml -b $(git branch --show-current) 
