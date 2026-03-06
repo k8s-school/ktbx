1a4d3d36701e7b8a95e6c834444a519ce1760525
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2026-03-06 15:35:16 +0100

    simulate error

--- e2e/run.sh
+++ e2e/run.sh
@@ -69,6 +69,9 @@ else
     echo "❌ Kind installation failed"
     exit 1
 fi
+    
+echo "❌ Fake error"
+exit 1
 
 if ktbx install kubectl; then
     log_step "kubectl_install" "passed" "Kubectl installed successfully"
